1851a2a30945dd317749b01822defabec3fd228d
Caleb.Wick <polarized.dino@gmail.com>
2022-07-20 18:08:24 +0700

    Update to use Campaign Struct

--- contracts/QuestReward.sol
+++ contracts/QuestReward.sol
@@ -16,15 +16,21 @@ contract QuestReward is Ownable, ReentrancyGuard {
     // Rewarder (address that is settable by admin)
     address public rewarder;
 
-    // Mapping of campaignID => User Address => TotalRewards
+    struct CampaignInfo {
+        // ERC20 Address for the Token Reward
+        address tokenReward;
+        // Amount of reward that is claimable
+        uint256 rewardPool;
+    }
+
+    // Mapping of campaignID to the campaignInfo
     // campaignID here can be anything, example for Learn and Earn is QuizID
+    mapping(string => CampaignInfo) public campaigns;
+
+    // Mapping of campaignID => User Address => TotalRewards
     mapping(string => mapping(address => uint256)) public userRewards;
-
-    // Mapping of campaignID to the ERC Token
-    mapping(string => address) public campaignTokens;
-
-    // Mapping of ERCToken to Rewards Available to be claimed
-    mapping(address => uint256) public tokenRewardPool;
+    // Mapping of campaignID => User Address => ClaimedRewards
+    mapping(string => mapping(address => uint256)) public claimedRewards;
 
     // Statistics
     // Total rewards rewarded for each token
@@ -38,7 +44,7 @@ contract QuestReward is Ownable, ReentrancyGuard {
 
     event SetRewarder(address indexed rewarder);
     event Reward(string campaignID, address indexed user, uint256 amount);
-    event Fund(address indexed token, address indexed user, uint256 amount);
+    event Fund(string indexed campaignID, address indexed user, uint256 amount);
     event Claim(address indexed user, string campaignID, uint256 amount);
     event Withdraw(address indexed token, uint256 amount);
 
@@ -75,48 +81,49 @@ contract QuestReward is Ownable, ReentrancyGuard {
     }
 
     // Function for funding the tokens to the reward pool
-    function fund(uint256 amount, address token) external {
-        // transfer specified amount from funder to this contract
-        ERC20(token).safeTransferFrom(_msgSender(), address(this), amount);
-        tokenRewardPool[token] += amount;
+    function fund(uint256 amount, string calldata campaignID) external {
+        address tokenAddress = campaigns[campaignID].tokenReward;
 
-        emit Fund(token, _msgSender(), amount);
+        // transfer specified amount from funder to this contract
+        ERC20(tokenAddress).safeTransferFrom(_msgSender(), address(this), amount);
+        campaigns[campaignID].rewardPool += amount;
+
+        emit Fund(campaignID, _msgSender(), amount);
     }
 
     // Function for withdrawing money from reward pool, in case we want to sunset this contract
-    function withdraw(address token) external onlyAdmin {
-        uint256 amount = tokenRewardPool[token];
+    function withdraw(string calldata campaignID) external onlyAdmin {
+        address token = campaigns[campaignID].tokenReward;
+        uint256 amount = campaigns[campaignID].rewardPool;
+
+        campaigns[campaignID].rewardPool = 0;
         ERC20(token).safeTransfer(_msgSender(), amount);
-        tokenRewardPool[token] = 0;
 
         emit Withdraw(token, amount);
     }
 
     // Function to be called when we want to reward user after finishing a campaign
-    function reward(uint256 amount, address user, string calldata campaignID, address tokenAddress) external onlyRewarder {
-        // Set the campaign-token mapping if it is not set before
-        if (campaignTokens[campaignID] == address(0)) {
-            campaignTokens[campaignID] = tokenAddress;
-        }
-
+    function reward(uint256 amount, address user, string calldata campaignID) external onlyRewarder {
         // transfer specified amount from funder to this contract
         userRewards[campaignID][user] += amount;
-        address rewardToken = campaignTokens[campaignID];
+        address token = campaigns[campaignID].tokenReward;
 
         // Stats
-        totalRewards[rewardToken] += amount;
+        totalRewards[token] += amount;
 
         emit Reward(campaignID, _msgSender(), amount);
     }
 
     // Function to be called when our user wants to claim their reward
     function claim(string calldata campaignID) external {
-        address rewardToken = campaignTokens[campaignID];
+        address rewardToken = campaigns[campaignID].tokenReward;
         uint256 amountToBeClaimed = userRewards[campaignID][_msgSender()];
 
-        require(amountToBeClaimed <= tokenRewardPool[rewardToken], 'reward pool is not enough');
+        require(amountToBeClaimed <= campaigns[campaignID].rewardPool, 'reward pool is not enough');
         userRewards[campaignID][_msgSender()] = 0;
-        tokenRewardPool[rewardToken] -= amountToBeClaimed;
+        claimedRewards[campaignID][_msgSender()] += amountToBeClaimed;
+        
+        campaigns[campaignID].rewardPool -= amountToBeClaimed;
 
         ERC20(rewardToken).safeTransfer(_msgSender(), amountToBeClaimed);
 
