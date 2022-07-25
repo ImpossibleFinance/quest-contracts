22a2010a429c60d08fbde898a8b503eac1df83b3
Caleb.Wick <polarized.dino@gmail.com>
2022-07-25 15:59:48 +0700

    Add happy path flow

--- test/test-QuestReward.ts
+++ test/test-QuestReward.ts
@@ -5,6 +5,7 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { Contract } from 'ethers'
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+const campaignID = 'campaign1'
 
 export const mineNext = async (): Promise<void> => {
   await network.provider.send('evm_mine') // mine next (+1 blockheight)
@@ -17,6 +18,7 @@ export default describe('QuestReward', function () {
   let owner: SignerWithAddress
   let admin: SignerWithAddress
   let rewarder: SignerWithAddress
+  let user: SignerWithAddress
   let QuestRewardContract: Contract
   let TestToken: Contract
 
@@ -31,6 +33,7 @@ export default describe('QuestReward', function () {
     owner = (await ethers.getSigners())[0]
     admin = (await ethers.getSigners())[1]
     rewarder = (await ethers.getSigners())[2]
+    user = (await ethers.getSigners())[3]
 
     const TestTokenFactory = await ethers.getContractFactory('GenericToken')
 
@@ -43,7 +46,7 @@ export default describe('QuestReward', function () {
     // deploy test token
     const QuestRewardFactory = await ethers.getContractFactory('QuestReward')
     QuestRewardContract = await QuestRewardFactory.deploy(admin.address)
-
+    mineNext()
     QuestRewardContract.connect(admin).setRewarder(rewarder.address)
     mineNext()
   })
@@ -63,7 +66,6 @@ export default describe('QuestReward', function () {
   })
 
   it('create campaigns correctly', async () => {
-    const campaignID = 'campaign1'
     await QuestRewardContract.connect(rewarder).createCampaign(
       TestToken.address,
       campaignID
@@ -73,18 +75,79 @@ export default describe('QuestReward', function () {
     expect(campaignInfo.tokenReward).to.equal(TestToken.address)
   })
 
-  //   it('funds tokens correctly', async () => {
-  //     const stakeAmt = [100, 250]
+  it('campaign can be created, funded, rewarded, claimed and withdrawed', async () => {
+    mineNext()
 
-  //     await TestToken.transfer(rewarder.address, '10000000000000000000000000000') // 10B tokens
-  //     await TestToken.transfer(rewarder.address, '10000000000000000000000000000') // 10B tokens
+    const REWARD_POOL = 1000
+    const USER_REWARD = 100
 
-  //     await TestToken.connect(rewarder).approve(
-  //       QuestRewardContract.address,
-  //       '1000'
-  //     )
+    // Transferring token to admin
+    await TestToken.transfer(admin.address, '10000000000000') // 1000 tokens
+    await TestToken.connect(admin).approve(QuestRewardContract.address, '1000')
+    mineNext()
 
-  //     await QuestRewardContract.fund(1000, 'campaign1')
-  //     mineNext()
-  //   })
+    const OwnerBalance = await TestToken.balanceOf(admin.address)
+
+    // Creating campaign
+    await QuestRewardContract.connect(rewarder).createCampaign(
+      TestToken.address,
+      campaignID
+    )
+    mineNext()
+
+    // Funding the campaign
+    await QuestRewardContract.connect(admin).fund(REWARD_POOL, campaignID)
+    mineNext()
+    const campaignInfo = await QuestRewardContract.campaigns(campaignID)
+    expect(campaignInfo.rewardPool).to.equal(REWARD_POOL)
+
+    // Rewarding the user
+    await QuestRewardContract.connect(rewarder).reward(
+      USER_REWARD,
+      user.address,
+      campaignID
+    )
+    mineNext()
+
+    const userData = await QuestRewardContract.userRewards(
+      campaignID,
+      user.address
+    )
+    expect(userData).to.equal(USER_REWARD)
+
+    const totalRewards = await QuestRewardContract.totalRewards(
+      TestToken.address
+    )
+    expect(totalRewards).to.equal(USER_REWARD)
+
+    // User Claiming the reward
+    await QuestRewardContract.connect(user).claim(campaignID)
+    mineNext()
+    expect(await TestToken.balanceOf(user.address)).to.equal(
+      USER_REWARD.toString()
+    )
+    // After claim, need to reinitialize
+    const campaignInfoClaim = await QuestRewardContract.campaigns(campaignID)
+    const userDataClaim = await QuestRewardContract.userRewards(
+      campaignID,
+      user.address
+    )
+    expect(campaignInfoClaim.rewardPool).to.equal(REWARD_POOL - USER_REWARD)
+    expect(userDataClaim).to.equal(0)
+    const claimedInfo = await QuestRewardContract.claimedRewards(
+      campaignID,
+      user.address
+    )
+    expect(claimedInfo).to.equal(USER_REWARD)
+
+    // Admin withdrawing idle rewards
+    await QuestRewardContract.connect(admin).withdraw(campaignID)
+    mineNext()
+    expect(await TestToken.balanceOf(admin.address)).to.equal(
+      OwnerBalance - USER_REWARD
+    )
+    // After withdraw, need to reinitialize
+    const campaignInfoWithdraw = await QuestRewardContract.campaigns(campaignID)
+    expect(campaignInfoWithdraw.rewardPool).to.equal(0)
+  })
 })
